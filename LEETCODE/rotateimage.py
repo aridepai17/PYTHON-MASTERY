a111446c1c488408917fbe2b72f739f707b9ba03
aridepai17 <advaithdepai26@gmail.com>
2025-05-30 12:49:40 +0530

    LeetCode 48

---
+++ LEETCODE/rotateimage.py
@@ -0,0 +1,27 @@
+# LEETCODE 48 
+'''
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+DO NOT allocate another 2D matrix and do the rotation.
+'''
+
+def rotate(matrix):
+    n = len(matrix)
+    
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+    for rows in matrix:
+        rows.reverse()
+        
+# Example usage:
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+rotate(matrix)
+print(matrix)
+# Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
