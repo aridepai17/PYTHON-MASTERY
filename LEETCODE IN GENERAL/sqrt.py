d729fbed34603e975150c7b1c3856a4af00cbb87
aridepai17 <advaithdepai26@gmail.com>
2025-05-30 14:01:55 +0530

    Made optimal binary search approach correct

--- LEETCODE IN GENERAL/sqrt.py
+++ LEETCODE IN GENERAL/sqrt.py
@@ -11,7 +11,7 @@ def mySqrt(x):
         return 0
     l, r = 1, x
     while l <= r:
-        mid = (l + r) // 2
+        mid = l + (r-l) // 2
         if mid * mid == x:
             return mid
         elif mid * mid < x:
