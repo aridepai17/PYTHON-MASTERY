4e6108e274781b9c198a6ca4aba8beee589c7f09
aridepai17 <advaithdepai26@gmail.com>
2025-05-30 17:49:14 +0530

    Transpose matrix problem

--- LEETCODE IN GENERAL/transposematrix.py
+++ LEETCODE IN GENERAL/transposematrix.py
@@ -10,13 +10,13 @@ def transpose(matrix):
     m = len(matrix)
     n = len(matrix[0])
     
-    transpose = []
+    t = []
     for i in range(n):
         temp = []
         for j in range(m):
             temp.append(matrix[j][i])
-        transpose.append(temp)
-    return transpose
+        t.append(temp)
+    return t
 
 # Example usage:
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
