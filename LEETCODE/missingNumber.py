229a6a9e215e3581f3640bb03039b0dece9ddc95
aridepai17 <advaithdepai26@gmail.com>
2025-05-31 09:06:24 +0530

    Made code more optimal

--- LEETCODE/missingNumber.py
+++ LEETCODE/missingNumber.py
@@ -3,7 +3,6 @@
 # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 def missingNumber(n, nums):
-    n = len(nums)
     total = n * (n + 1) // 2
     sumarray = sum(nums)
     missing = total - sumarray
