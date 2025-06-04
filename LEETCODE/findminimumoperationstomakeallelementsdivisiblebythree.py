722716ed726908b091453820879f1f36779e02af
aridepai17 <advaithdepai26@gmail.com>
2025-06-04 18:50:52 +0530

    Find minimum operations to make all elements divisible by three

--- LEETCODE/findminimumoperationstomakeallelementsdivisiblebythree.py
+++ LEETCODE/findminimumoperationstomakeallelementsdivisiblebythree.py
@@ -1,6 +1,6 @@
 # LEETCODE 3190
 
-# You are given an integer array nums. In one operation, you can add or subtract 1 from any elemtent of nums.
+# You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
 # Return the minimum number of operations to make all elements of nums divisible by 3.
 
 def minimumOperations(nums):
